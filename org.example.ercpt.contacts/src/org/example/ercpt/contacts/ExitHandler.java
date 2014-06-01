c3758365cb19705a2b0e15e05ae7825914126451
Michael Scharf <github@scharf.gr>
2014-06-01 03:42:47 +0200

    Lab: Create a Handler

---
+++ org.example.ercpt.contacts/src/org/example/ercpt/contacts/ExitHandler.java
@@ -0,0 +1,13 @@
+ 
+package org.example.ercpt.contacts;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.IWorkbench;
+
+public class ExitHandler {
+	@Execute
+	public void execute(IWorkbench workbench) {
+		workbench.close();
+	}
+		
+}

b97bcbd53daab4b49c1b8fe76d82f28e5148d264
Michael Scharf <github@scharf.gr>
2014-06-03 03:58:12 +0200

    Lab: Add Databinding

--- org.example.ercpt.contacts.views.details/src/org/example/ercpt/contacts/views/details/DetailsView.java
+++ org.example.ercpt.contacts.views.details/src/org/example/ercpt/contacts/views/details/DetailsView.java
@@ -4,16 +4,25 @@ import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.beans.PojoObservables;
+import org.eclipse.core.databinding.observable.Realm;
+import org.eclipse.core.databinding.observable.value.WritableValue;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+import org.example.ercpt.contacts.model.Contact;
 
 public class DetailsView {
+	private DataBindingContext dbc;
+	private WritableValue contactValue;
 	private Text firstName;
 	private Text lastName;
 	private Label lblEmail;
@@ -27,20 +36,35 @@ public class DetailsView {
 	@PostConstruct
 	public void postConstruct(Composite parent) {
 		parent.setLayout(new GridLayout(2, false));
+		Realm.runWithDefault(SWTObservables.getRealm(Display.getDefault()),
+				new Runnable() {
+
+					@Override
+					public void run() {
+						dbc = new DataBindingContext();
+						contactValue = new WritableValue();
+					}
+				});
 		
 		Label nameLabel = new Label(parent, SWT.NONE);
 		nameLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		nameLabel.setText("First Name");
-		
 		firstName = new Text(parent, SWT.BORDER);
 		firstName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		dbc.bindValue(SWTObservables.observeText(firstName, SWT.Modify),
+				PojoObservables.observeDetailValue(contactValue, Contact.FIELD_FIRST_NAME,
+						String.class));
+		
 		
 		Label lblLastName = new Label(parent, SWT.NONE);
 		lblLastName.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblLastName.setText("Last Name");
-		
 		lastName = new Text(parent, SWT.BORDER);
 		lastName.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		dbc.bindValue(SWTObservables.observeText(lastName, SWT.Modify),
+				PojoObservables.observeDetailValue(contactValue, Contact.FIELD_LAST_NAME,
+						String.class));
+		
 		
 		lblEmail = new Label(parent, SWT.NONE);
 		lblEmail.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
@@ -48,20 +72,17 @@ public class DetailsView {
 		
 		email = new Text(parent, SWT.BORDER);
 		email.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		dbc.bindValue(SWTObservables.observeText(email, SWT.Modify),
+				PojoObservables.observeDetailValue(contactValue, Contact.FIELD_EMAIL,
+						String.class));
 	}
 
 	@Inject
-	public void setSelection(@Optional @Named(IServiceConstants.ACTIVE_SELECTION) String selection) {
+	public void setSelection(@Optional @Named(IServiceConstants.ACTIVE_SELECTION) Contact contact) {
 		if(firstName==null || firstName.isDisposed())
 			return;
-		String[] strings = new String[]{"",""};
-		if(selection!=null) {
-			String[] s = selection.split("\\s+");
-			if(s.length == 2) {
-				strings = s;
-			}
+		if (contact != null) {
+			contactValue.setValue(contact);
 		}
-		firstName.setText(strings[0]);
-		lastName.setText(strings[1]);
 	}
 }
--- org.example.ercpt.contacts/src/org/example/ercpt/contacts/ListView.java
+++ org.example.ercpt.contacts/src/org/example/ercpt/contacts/ListView.java
@@ -3,12 +3,21 @@ package org.example.ercpt.contacts;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
+import org.eclipse.core.databinding.beans.BeansObservables;
+import org.eclipse.core.databinding.observable.map.IObservableMap;
 import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.jface.databinding.viewers.ObservableListContentProvider;
+import org.eclipse.jface.databinding.viewers.ObservableMapLabelProvider;
+import org.eclipse.jface.layout.GridLayoutFactory;
+import org.eclipse.jface.layout.TableColumnLayout;
+import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -21,20 +30,55 @@ public class ListView {
 	private ESelectionService selectionService;
 	@PostConstruct
 	public void postConstruct(Composite parent, ContactsRepository contacts) {
-		parent.setLayout(new GridLayout(1, false));
-		tableViewer = new TableViewer(parent);
-		for(Contact contact :contacts.getAllContacts()) {
-			tableViewer.add(contact.getFirstName()+ " " + contact.getLastName());
-			
-		}
-		tableViewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
-		tableViewer.addSelectionChangedListener(new ISelectionChangedListener() {
-			@Override
-			public void selectionChanged(SelectionChangedEvent event) {
-				IStructuredSelection selection = (IStructuredSelection) event.getSelection();
-				selectionService.setSelection(selection.getFirstElement());
-			}
-		});
+		// Table composite (because of TableColumnLayout)
+		final Composite tableComposite = new Composite(parent, SWT.NONE);
+		tableComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true,
+				true));
+		final TableColumnLayout tableColumnLayout = new TableColumnLayout();
+		tableComposite.setLayout(tableColumnLayout);
+
+		// Table viewer
+		tableViewer = new TableViewer(tableComposite, SWT.FULL_SELECTION);
+		tableViewer.getTable().setHeaderVisible(true);
+
+		// first name column
+		final TableViewerColumn firstNameColumn = new TableViewerColumn(
+				tableViewer, SWT.NONE);
+		firstNameColumn.getColumn().setText("First Name");
+		tableColumnLayout.setColumnData(firstNameColumn.getColumn(),
+				new ColumnWeightData(40));
+
+		// last name column
+		final TableViewerColumn lastNameColumn = new TableViewerColumn(
+				tableViewer, SWT.NONE);
+		lastNameColumn.getColumn().setText("Last Name");
+		tableColumnLayout.setColumnData(lastNameColumn.getColumn(),
+				new ColumnWeightData(40));
+
+		ObservableListContentProvider contentProvider = new ObservableListContentProvider();
+
+		tableViewer.setContentProvider(contentProvider);
+
+		IObservableMap[] attributes = BeansObservables.observeMaps(
+				contentProvider.getKnownElements(), Contact.class,
+				new String[] { "firstName", "lastName" });
+		tableViewer
+				.setLabelProvider(new ObservableMapLabelProvider(attributes));
+
+		tableViewer.setInput(contacts.getAllContacts());
+
+		GridLayoutFactory.fillDefaults().generateLayout(parent);
+
+		tableViewer
+				.addSelectionChangedListener(new ISelectionChangedListener() {
+					@Override
+					public void selectionChanged(SelectionChangedEvent event) {
+						IStructuredSelection selection = (IStructuredSelection) event
+								.getSelection();
+						selectionService.setSelection(selection
+								.getFirstElement());
+					}
+				});
 	}
 	
 	
